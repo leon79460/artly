aeb2d8b21446898adb9e38b5ad13f6fbb84637f8
Leon <hogaifauddinbhuiyan100@gmail.com>
2025-10-21 17:23:20 +0600

    header check

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exdos - Creative Agency HTML Template </title>
+    <title>Artly - Creative Agency HTML Template </title>
 
     <?php wp_head(); ?>
  
